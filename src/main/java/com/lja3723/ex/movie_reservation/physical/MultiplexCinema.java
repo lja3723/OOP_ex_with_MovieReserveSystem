91db986c4be9524e29d309cd38d7d32abc243f98
lja3723 <lja3723@gmail.com>
2022-04-24 19:40:37 +0900

    MultiplexCinema finance 관리 추가

--- src/main/java/com/lja3723/ex/movie_reservation/physical/MultiplexCinema.java
+++ src/main/java/com/lja3723/ex/movie_reservation/physical/MultiplexCinema.java
@@ -5,8 +5,8 @@ import com.lja3723.ex.movie_reservation.value.Money;
 
 public class MultiplexCinema {
     private final String name;
-    private Money finance; //재정
     private final List<Theatre> theatres;
+    private Money finance; //재정
 
     public MultiplexCinema(String name, Money finance, List<Theatre> theatres) {
         this.name = name;
@@ -24,5 +24,17 @@ public class MultiplexCinema {
 
     public final List<Theatre> getTheatres() {
         return theatres;
+    }
+
+    public void getMoney(Money getAmount) {
+        finance = finance.plus(getAmount);
+    }
+
+    public boolean spentMoney(Money spendAmount) {
+        if (finance.isLessThen(spendAmount)) {
+            return false;
+        }
+        finance = finance.minus(spendAmount);
+        return true;
     }
 }
